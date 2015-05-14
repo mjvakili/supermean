92f5a0f8223c8331e65e40ee6df29419981ca666
MJ <mv1003@nyu.edu>
2015-05-14 18:28:04 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import pyfits as pf
 from mean import stuff
 
+epsilon = .1
+H = 5
 
 a = pf.open("wfc3_f160w_clean_central100.fits")[0]
-h = stuff(a, 3 , min_iter=1, max_iter=30, check_iter=1, tol=1.e-14)
+h = stuff(a, H , epsilon ,min_iter=1, max_iter=30, check_iter=1, tol=1.e-14)
 
