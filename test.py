ca99f8f656dfec871ec2bb585293adfe6dd27e1f
MJ <mv1003@nyu.edu>
2015-05-17 20:14:18 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from mean import stuff
 epsilon = .1
 H = 5
 
-a = pf.open("wfc3_f160w_clean_central100.fits")[0]
-h = stuff(a, H , epsilon ,min_iter=1, max_iter=30, check_iter=1, tol=1.e-14)
+a = pf.open("wfc3_f160w_clean_central100.fits")
+data, mask = a[0].data, a[1].data
+h = stuff(data, mask , H , epsilon ,min_iter=1, max_iter=30, check_iter=1, tol=1.e-14)
 
