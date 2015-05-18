d9beb2babe5bf2d1e61adff37eb035cd578bbf4e
MJ <mv1003@nyu.edu>
2015-05-18 18:04:53 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import pyfits as pf
+import numpy as np
 from mean import stuff
 
-epsilon = .1
-H = 5
 
-a = pf.open("wfc3_f160w_clean_central100.fits")
-data, mask = a[0].data, a[1].data
-h = stuff(data, mask , H , epsilon ,min_iter=1, max_iter=30, check_iter=1, tol=1.e-14)
+a  = pf.open("wfc3_f160w_clean_central100.fits")[0].data
+b  = pf.open("wfc3_f160w_clean_central100.fits")[1].data
+cx = np.loadtxt("wfc3_f160w_clean_central100_matchedfilterpoly_cx.txt")
+cy = np.loadtxt("wfc3_f160w_clean_central100_matchedfilterpoly_cy.txt")
+
+H = 5
+epsilon = .1
+
+h = stuff(a, cx, cy, b, H, epsilon, min_iter=1, max_iter=20, check_iter=1, tol=.01)
 
