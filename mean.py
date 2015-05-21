fixed masking

mean.py
@@ -7,26 +7,32 @@ import scipy.optimize as op
 from scipy import ndimage
 f  = .05
 g  = .01
-fl = 1e-6
+fl = 1e-5
 
 class stuff(object):
    
      def __init__(self, data, cx, cy, mask, H = 3, epsilon = .01 , min_iter=5, max_iter=10, check_iter=5 , tol=1.e-8):
 
-        """ inputs of the code: NxD data matrix and NxD uncertainty matrix;
+        """ inputs of the code: NxD data matrix and NxD mask matrix;
+            data contains images of stars, and mask contains questionable
+            pixels in each image.
             N = the number of stars
-            D = the number of pixels in each star                             
+            D = the number of pixels in each patch
+            H = upsampling factor
+            cx, cy = centroiding offsets
+                             
         """
 
         self.N = data.shape[0]           #number of observations
-        self.D = data.shape[1]           #input dimensionality, dimension of each observed data point
+        self.D = data.shape[1]           #input dimensionality
         self.H = H                       #upsampling factor
         self.epsilon = epsilon           #smoothness parameter                                  
         self.data = np.atleast_2d(data)  #making sure the data has the right dimension
-        self.mask = mask                 #data quality
+        self.mask = np.atleast_2d(mask)  #making sure the mask has the right dimension
         self.dx = cx                     #list of centroid offsets x
         self.dy = cy                     #list of centroid offsets y   
         self.M = int(self.D**.5)
+         
         """ outputs of the code:
                                  H*H*D-dimensional mean vector:  X
                                  N-dimensional flux vector:      F
@@ -47,6 +53,11 @@ class stuff(object):
         
         
      def initialize(self):
+
+        """
+        initializing the parameters
+        """         
+        
         m = int((self.D)**.5)
         self.d2d = self.data.reshape(self.N , m , m)
         self.dm = np.zeros((self.N, self.D))
@@ -54,7 +65,7 @@ class stuff(object):
         for i in range(self.N):
                           
           self.B[i]   =  np.array([self.d2d[i,m/2-4:m/2+5,-1:].mean(),self.d2d[i,m/2-4:m/2+5,:1].mean(),self.d2d[i,:1,m/2-4:m/2+5].mean(),self.d2d[i,-1:,m/2-4:m/2+5].mean()]).mean()
-             
+          
           self.dm[i]  =  self.data[i]-self.B[i]
           self.dm    -= self.dm.min()
           self.F[i]   =  np.sum(self.dm[i])
@@ -62,9 +73,7 @@ class stuff(object):
           shifted = shifter.shifter(self.dm[i], self.dx[i], self.dy[i])
           obs = ndimage.interpolation.zoom(shifted.reshape(25,25), self.H, output = None, order=3, mode='constant', cval=0.0, prefilter=True).flatten()
           
-          
-          X += obs.flatten()   
-          
+        X += obs.flatten()   
         X /= self.N
         self.lnX = np.log(X)
         
@@ -87,11 +96,12 @@ class stuff(object):
         grad = 2.*self.epsilon*(4.*Z - c).flatten()
         grad = grad*self.X               
         
-        #grad = np.zeros_like(self.X)
+        
         for p in range(self.N):
          Kp = sampler.imatrix_new(self.M, self.H, self.dx[p], self.dy[p])
          modelp = self.F[p]*(self.X+fl).dot(Kp) + self.B[p]
          ep = self.data[p] - modelp
+         ep[self.mask[p]!=0] = 0   #excluding flagged pixels from contributing to gradient_X
          varp = f + g*np.abs(modelp)
          gradp = -1.*self.F[p]*Kp
          
@@ -121,6 +131,7 @@ class stuff(object):
          nmodelp = np.dot(self.X+fl,Kp)
          modelp = self.F[p]*nmodelp + self.B[p]
          residualp = self.data[p] - modelp
+         residualp[self.mask[p]!=0] = 0   #excluding flagged pixels from contributing to gradient_X
          varp = f + g*np.abs(modelp)
          gradp = -1.*nmodelp
          gainp = (g/2.)*nmodelp*(varp**-1. - residualp**2./varp**2.)
@@ -129,8 +140,6 @@ class stuff(object):
         return grad
       
      def grad_B(self, params, *args):
-        
-        """Gradient w.r.t B """
 
         self.lnX, self.F = args
         self.B = params
@@ -143,6 +152,7 @@ class stuff(object):
          modelp = self.F[p]*np.dot(self.X+fl,Kp) + self.B[p]
          varp = f+g*np.abs(modelp)
          residualp = self.data[p] - modelp
+         residualp[self.mask[p]!=0] = 0   #excluding flagged pixels from contributing to gradient_X
          gainp = - (g/2.)*(residualp**2./varp**2.) + (g/2.)*(varp**-1.)
          gainp[modelp<0] *= -1.   #var=f+g|model| to account for numerical artifacts when sr model is sampled at the data grid   
          grad[p] = -1.*np.sum(residualp/varp) + np.sum(gainp)      
@@ -168,13 +178,13 @@ class stuff(object):
 
      def bfgs_lnX(self):
         x = op.fmin_l_bfgs_b(self.func_lnX,x0=self.lnX, fprime = self.grad_lnX,args=(self.F, self.B), approx_grad = False, \
-                              bounds = None, m=10, factr=100., pgtol=1e-04, epsilon=1e-04, maxfun=20)
+                              bounds = None, m=10, factr=100., pgtol=1e-04, epsilon=1e-04, maxfun=60)
         print x
         self.lnX  = x[0]
 
      def bfgs_F(self):
         x = op.fmin_l_bfgs_b(self.func_F,x0=self.F, fprime = self.grad_F,args=(self.lnX, self.B), approx_grad = False, \
-                              bounds = None, m=10, factr=100., pgtol=1e-04, epsilon=1e-04, maxfun=20)
+                              bounds = None, m=10, factr=100., pgtol=1e-04, epsilon=1e-04, maxfun=30)
         print x
         self.F  = x[0]
 
@@ -193,11 +203,10 @@ class stuff(object):
        for i in range(self.N):
          Ki = sampler.imatrix_new(self.M, self.H, self.dx[i], self.dy[i])
          model_i = self.F[i]*np.dot(self.X+fl, Ki) + self.B[i]
-         model_square = model_i
-         data_square = self.data[i,:]
+         residual_i = self.data[i] - model_i
+         residual_i[self.mask[i]!=0] = 0   #excluding flagged pixels from contributing to NLL
          var_i = f + g*np.abs(model_i)
-         
-         nll += 0.5*np.sum(((model_square - data_square)**2.)/var_i) + .5*np.sum(np.log(var_i))
+         nll += 0.5*np.sum(((residual_i)**2.)/var_i) + .5*np.sum(np.log(var_i))
        return nll
      
      def update(self, max_iter, check_iter, min_iter, tol):
@@ -224,9 +233,9 @@ class stuff(object):
             obj = self.nll()
             print "NLL after B-step", obj
             
-            np.savetxt("wfc_mean_iter_%d.txt"%(i+1)       , self.lnX ,fmt='%.12f')
-            np.savetxt("wfc_flux_iter_%d.txt"%(i+1)       , self.F ,fmt='%.12f')
-            np.savetxt("wfc_background_iter_%d.txt"%(i+1) , self.B ,fmt='%.12f')
+            np.savetxt("noregwfc_mean_iter_%d.txt"%(i+1)       , self.lnX ,fmt='%.12f')
+            np.savetxt("noregwfc_flux_iter_%d.txt"%(i+1)       , self.F ,fmt='%.12f')
+            np.savetxt("noregwfc_background_iter_%d.txt"%(i+1) , self.B ,fmt='%.12f')
             chi.append(obj)
                         
 